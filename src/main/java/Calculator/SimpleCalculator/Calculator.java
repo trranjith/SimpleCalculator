da7aa834dd2b02600023127324e888fcfebce1d8
Ranjith <r.thirumalai.ramesh@accenture.com>
2017-12-21 09:26:50 +0530

    Adding project to Repository

---
+++ src/main/java/Calculator/SimpleCalculator/Calculator.java
@@ -0,0 +1,34 @@
+package Calculator.SimpleCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+class ZeroDivisionException extends Exception{
+	public ZeroDivisionException(){
+		super();
+	}
+}
+public class Calculator 
+{
+    public double doAdd(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+    public double doSub(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+    public double doMul(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+    public double doDiv(double num1, double num2) throws ZeroDivisionException
+    {
+        if(num2 == 0){
+        	throw new ZeroDivisionException();
+        }
+    	return num1 / num2;
+    }
+    
+}
